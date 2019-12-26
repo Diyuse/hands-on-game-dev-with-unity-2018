aa121f216f1fb3b33194655c0c59e30ca5867afe
diyuse <daniel.yan.wu@gmail.com>
2019-12-26 18:33:14 -0500

    Added stat system

---
+++ Assets/PlayerController/Scripts/CharacterDataTemplate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MyCompany.MyGame.Player
+{
+    [CreateAssetMenu(fileName = "New CharacterDataTemplate", menuName = "MyCompany/Data/Create Character Data Template")]
+    public class CharacterDataTemplate : ScriptableObject
+    {
+        [SerializeField] private CharacterData characterData;
+
+        public CharacterData Data
+        {
+            get { return characterData; }
+        }
+    }
+}
---
+++ Assets/Weapons/Scripts/WeaponDataTemplate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MyCompany.MyGame.Weapons
+{
+    [CreateAssetMenu(fileName ="New WeaponDataTemplate", menuName ="MyCompany/Data/Create Weapon Data Template")]
+    public class WeaponDataTemplate : ScriptableObject
+    {
+        [SerializeField] private WeaponData weaponData;
+
+        public WeaponData WeaponData
+        {
+            get { return weaponData; }
+        }
+    }
+}
