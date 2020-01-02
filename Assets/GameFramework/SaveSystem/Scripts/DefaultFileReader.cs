b869dc0d1c93ed4d204d1a83bfa916a920a060bc
diyuse <daniel.yan.wu@gmail.com>
2020-01-01 22:33:04 -0500

    Added functionality for saving

---
+++ Assets/GameFramework/SaveSystem/Scripts/DefaultFileReader.cs
@@ -0,0 +1,20 @@
+﻿using MyCompany.GameFramework.SaveSystem.Interfaces;
+using System.IO;
+
+namespace MyCompany.GameFramework.SaveSystem
+{
+    public class DefaultFileReader : IFileReader
+    {
+        public byte[] Read(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.ReadAllBytes(path);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
