69b19b0c1a4377fa7a33606ba48f2bf058b1f210
diyuse <daniel.yan.wu@gmail.com>
2020-01-03 18:57:32 -0500

    Implemented an enemy that shoots projectiles

---
+++ Assets/MyGame/Enemies/Scripts/BigBoss.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using MyCompany.GameFramework.EnemyAI;
+using MyCompany.GameFramework.EnemyAI.Interfaces;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace MyCompany.MyGame.Enemies
+{
+    public class BigBoss : MonoBehaviour
+    {
+        protected IMovementBehavior movementBehavior;
+
+        protected Dictionary<IActionCondition, IEnemyAbility> abilities =
+            new Dictionary<IActionCondition, IEnemyAbility>();
+
+        protected NavMeshAgent agent;
+        protected GameObject player;
+        [SerializeField] protected GameObject projectilePrefab;
+
+        private void Awake()
+        {
+            agent = gameObject.GetComponent<NavMeshAgent>();
+            player = GameObject.FindWithTag("Player");
+            
+            /* Initializing interfaces */
+            movementBehavior = new RoamBehavior(agent, 8);
+            SetupAbilities();
+        }
+
+        private void SetupAbilities()
+        {
+            var transform1 = transform;
+            BurstAttack ba = new BurstAttack(15, transform1, projectilePrefab);
+            ba.onBegin += () => { agent.isStopped = true; };
+            ba.onComplete += () => { agent.isStopped = false; };
+            RangeCondition rc = new RangeCondition(transform1, player.transform, 12);
+            abilities.Add(rc, ba);
+        }
+
+        private void Update()
+        {
+            if (!agent.hasPath)
+            {
+                movementBehavior.SetNextTargetPosition();
+            }
+
+            CheckConditions();
+        }
+
+        private void CheckConditions()
+        {
+            foreach (var kvp in abilities)
+            {
+                if (kvp.Key.CheckCondition())
+                {
+                    kvp.Value.UseAbility();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/MyGame/Enemies/Scripts/BurstAttack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using MyCompany.GameFramework.Coroutines;
+using MyCompany.GameFramework.EnemyAI.Interfaces;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MyCompany.MyGame.Enemies
+{
+    public class BurstAttack : IEnemyAbility
+    {
+        private readonly float burstIterations;
+        private Transform emissionPoint;
+        private GameObject bulletPrefab;
+
+        private float duration;
+        private int steps;
+
+        private bool inUse;
+
+        public event Action onBegin;
+        public event Action onComplete;
+
+        public BurstAttack(int burstCount, Transform emissionPoint, GameObject bulletPrefab, float duration = 2.0f,
+            int steps = 36)
+        {
+            burstIterations = Mathf.PI * 2 * burstCount;
+            this.emissionPoint = emissionPoint;
+            this.bulletPrefab = bulletPrefab;
+            this.duration = duration;
+            this.steps = steps;
+        }
+
+        public void UseAbility()
+        {
+            if (inUse)
+            {
+                return;
+            }
+
+            if (onBegin != null)
+            {
+                onBegin.Invoke();
+            }
+
+            inUse = true;
+            CoroutineHelper.RunCoroutine(Burst);
+        }
+
+        private IEnumerator Burst()
+        {
+            float timer = burstIterations;
+            float mult = timer / duration;
+            float t = 0.0f;
+            while (t < duration)
+            {
+                t += duration / steps;
+                Vector3 point = new Vector3(emissionPoint.position.x + Mathf.Sin(t * mult) * 2, 0.2f,
+                    emissionPoint.position.z + Mathf.Cos(t * mult) * 2);
+                Vector3 dir = point - emissionPoint.position;
+                dir.Normalize();
+                dir = emissionPoint.position + dir * 10;
+                dir.y = 0.2f;
+                GameObject ins = GameObject.Instantiate(bulletPrefab, point, Quaternion.identity);
+                ins.transform.LookAt(dir);
+                ins.GetComponent<Rigidbody>().velocity = ins.transform.forward * 7;
+                yield return new WaitForSeconds(duration/steps);
+            }
+
+            inUse = false;
+            if (onComplete != null)
+            {
+                onComplete.Invoke();
+            }
+        }
+    }
+}
